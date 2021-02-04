dc02be446088242e6794041b6eb1bc66cf2f4714
Andrew Farley <amf7crazy@gmail.com>
2021-02-04 18:26:44 -0500

    Added loading anim and button for auto-detect

--- extension/comms.js
+++ extension/comms.js
@@ -1,5 +1,7 @@
 const API_URL = "http://134.122.43.254:3030/process";
 
+document.getElementById('loading').style.display = "none";
+
 document.getElementById("data").addEventListener("keyup", (event) => {
   if (event.keyCode == 13) {
     event.preventDefault();
@@ -11,11 +13,14 @@ document.getElementById("sendData").addEventListener("click", sendRequest);
 document.getElementById("clear").addEventListener("click", clear);
 
 function sendData(data) {
+	document.getElementById('loading').style.display = "";
+
   $.ajax({
     type: "POST",
     url: API_URL,
     data: {'text': data},
     success: (res) => {
+    	document.getElementById('loading').style.display = "none";
       console.log(`Success! ${res}`);
       data = res.split(',');
       if (data[0] == "0") { //is fake
@@ -27,6 +32,7 @@ function sendData(data) {
       openTab("output");
     },
     error: (a, b, c) => {
+    	document.getElementById('loading').style.display = "none";
       console.log(`Error: ${c}`);
     }
   });
--- extension/pageComms.js
+++ extension/pageComms.js
@@ -1,3 +1,5 @@
+document.getElementById("autoDetect").addEventListener("click", autoDetect);
+
 function handleBody(text) {
   let out = "";
   let orig = text.split('\n');
@@ -10,9 +12,12 @@ function handleBody(text) {
   return out;
 }
 
-chrome.tabs.getSelected(null, (tab) => {
-  chrome.tabs.sendRequest(tab.id, {}, (res) => {
-    console.log(handleBody(res.html));
-    sendData(handleBody(res.html));
+function autoDetect() {
+  console.log("detecting");
+  chrome.tabs.getSelected(null, (tab) => {
+    chrome.tabs.sendRequest(tab.id, {}, (res) => {
+      console.log(handleBody(res.html));
+      sendData(handleBody(res.html));
+    });
   });
-});
+}
